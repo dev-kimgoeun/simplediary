1ed6be961ac154332228bebb8236cffe494ea87d
kim gon <ak0150a01@gmail.com>
2024-02-08 12:57:42 +0900

    list 작성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-
+// import React, {useState, useEffect} from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList.js';
 
-function App() {
+
+const dummyList = [
+  {
+    id : 1,
+    author : "김고은",
+    content : "안뇽",
+    emotion : 4,
+    created_date : new Date().getTime()
+  },
+  {
+    id : 2,
+    author : "김고은",
+    content : "안뇽2",
+    emotion : 4,
+    created_date : new Date().getTime()
+  },
+  {
+    id : 3,
+    author : "김고은",
+    content : "안뇽3",
+    emotion : 4,
+    created_date : new Date().getTime()
+  },
+  {
+    id : 4,
+    author : "김고은",
+    content : "안뇽4",
+    emotion : 4,
+    created_date : new Date().getTime()
+  }
+]
+const App = () => {
   return (
     <div className="App">
-   
    <DiaryEditor/>
+   <DiaryList diaryList = {dummyList}/>
     </div>
   );
-}
+};
 
 export default App;
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import {useState} from "react";
+import React, {useRef, useState} from "react";
 
 const DiaryEditor = () =>{
+
+    const authorInput = useRef();
+    const contentInput = useRef();
 
     const [state, setState] = useState({
         author:"",
@@ -19,7 +22,16 @@ const DiaryEditor = () =>{
     };
    
     const handleSubmit = () =>{
-        console.log(state);
+        if(state.author.length < 1){
+            authorInput.current.focus();
+            //focus
+            return;
+        }
+        if(state.content.length < 5){
+            contentInput.current.focus();
+            //focus
+            return;
+        }
         alert("저장성공!");
     };
 
@@ -27,12 +39,14 @@ const DiaryEditor = () =>{
         <h2>오늘의 일기</h2>
         <div>
             <input 
+            ref = {authorInput}
             name="author"
             value={state.author} 
             onChange={handleChangeState}/>
         </div>
         <div>
             <textarea 
+            ref={contentInput}
             name="content"
             value={state.content} 
             onChange={handleChangeState} />
