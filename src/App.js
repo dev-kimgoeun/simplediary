19b40a8bb71785d5940de8092eb592e77ed528a9
kim gon <ak0150a01@gmail.com>
2024-02-08 15:10:09 +0900

    댓글 수정 삭제 버튼

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
 // import React, {useState, useEffect} from 'react';
+import {useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
-import DiaryList from './DiaryList.js';
+import DiaryList from './DiaryList';
 
 
-const dummyList = [
-  {
-    id : 1,
-    author : "김고은",
-    content : "안뇽",
-    emotion : 4,
-    created_date : new Date().getTime()
-  },
-  {
-    id : 2,
-    author : "김고은",
-    content : "안뇽2",
-    emotion : 4,
-    created_date : new Date().getTime()
-  },
-  {
-    id : 3,
-    author : "김고은",
-    content : "안뇽3",
-    emotion : 4,
-    created_date : new Date().getTime()
-  },
-  {
-    id : 4,
-    author : "김고은",
-    content : "안뇽4",
-    emotion : 4,
-    created_date : new Date().getTime()
-  }
-]
-const App = () => {
+function App() {
+
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author, 
+      content,
+      emotion,
+      created_date,
+      id : dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) => it.id === targetId ? {...it, content:newContent} : it)
+    );
+  };
+
   return (
     <div className="App">
-   <DiaryEditor/>
-   <DiaryList diaryList = {dummyList}/>
+   <DiaryEditor onCreate={onCreate}/>
+   <DiaryList onEdit={onEdit} onRemove={onRemove}  diaryList = {data}/>
     </div>
   );
 };
